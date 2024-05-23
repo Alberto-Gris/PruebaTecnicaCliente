2442ccf41af4d018e03d21a487a3f5e14b7d4b01
AlbertoHPRO <albertogris13@gmail.com>
2024-05-23 15:03:47 -0600

    Avance de listado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useFetch,usePost } from "./useFetch";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 export function App() {
 
@@ -11,17 +13,37 @@ export function App() {
                     <div className="col-md-4 offset-4">
                         <div className="d-grid mx-auto">
                             <button className="btn btn-dark">
-                                Hola
+                                Nuevo Usuario <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                                </svg>
                             </button>
                         </div>
                     </div>
                 </div>
-                
             </div>
+            <div class="row">
+                <div class="col-4">
+                    <div class="list-group" id="list-tab" role="tablist">
+                        {loading && <li>Loading...</li>}
+                        {data?.map((user)=>(<a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home" key={user.id}>{user.name}</a>))}
+                        <a >Inicio</a>
+                        <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Perfil</a>
+                    <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Mensajes</a>
+                    <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Configuración</a>
+                    </div>
+                </div>
+                <div class="col-8">
+                    <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">1</div>
+                    <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">2</div>
+                    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">3</div>
+                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">4</div>
+                    </div>
+                </div>
+                </div>
             <div>
                 <ul>
-                    {loading && <li>Loading...</li>}
-                    {data?.map((user)=>(<li key={user.id}>{user.name}</li>))}
+
                 </ul>
             </div>
         </div>
